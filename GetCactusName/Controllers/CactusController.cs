0629a482d0f59b58fd4376dc941398fb2ce6f0d9
Ian Brown <iancarverbrown@gmail.com>
2021-07-24 23:49:38 -0400

    Add project files.

---
+++ GetCactusName/Controllers/CactusController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GetCactusName.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CactusController : ControllerBase
+    {
+
+        [HttpGet("GetCactus")]
+        public IActionResult GetCactus()
+        {
+            var cacti = new List<Species>();
+            cacti.Add(new Species("Prickly Pear", 10));
+            cacti.Add(new Species("Saguaro", 8));
+            cacti.Add(new Species("Ferocactus", 15));
+            cacti.Add(new Species("Elephant Pear", 10));
+            cacti.Add(new Species("Purple", 8));
+            cacti.Add(new Species("Watermelon", 15));
+            cacti.Add(new Species("Barrel", 10));
+            cacti.Add(new Species("Stinky Cactus", 8));
+            cacti.Add(new Species("Dick Cactus", 15));
+
+            var r = new Random();
+            return Ok(cacti[r.Next(0, cacti.Count - 1)]);
+        }
+
+    }
+
+    public class Species
+    {
+        public Species(string name, int weight)
+        {
+            Name = name;
+            Weight = weight;
+        }
+
+        public string Name { get; set; }
+        public int Weight { get; set; }
+    }
+
+}
